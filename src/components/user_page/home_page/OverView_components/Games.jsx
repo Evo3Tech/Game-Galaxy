0dc88417f61959614b16e3af967692f2b7997f26
hamdi touballi <hamditouballi2002@gmail.com>
2024-11-29 03:22:00 +0100

    add css for cards games

--- src/components/user_page/home_page/OverView_components/Games.jsx
+++ src/components/user_page/home_page/OverView_components/Games.jsx
@@ -1,16 +1,18 @@
+import { useEffect, useState } from "react";
 import Game from "./Game";
 
 export default function Games(){
+    const [data,setdata]=useState([]);
+    useEffect(()=>{
+        fetch('http://localhost:1231/all_Games')
+        .then((data)=>data.json())
+        .then((res)=>setdata(res))
+    },[])
     return(
         <div className="games">
-            <Game/>
-            <Game/>
-            <Game/>
-            <Game/>
-            <Game/>
-            <Game/>
-            <Game/>
-            <Game/>
+            {data.map((d)=>{
+              return  <Game game={d}/>
+            })}
         </div>
     )
 }
