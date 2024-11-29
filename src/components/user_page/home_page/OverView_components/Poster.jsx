e7790e59f53de2e7d133ff27a5c712354ff4c050
Cherkaoui El Wafi <elwaficherkaoui@gmail.com>
2024-11-29 15:10:38 +0100

    Change home_page layout,animations, improve posters

--- src/components/user_page/home_page/OverView_components/Poster.jsx
+++ src/components/user_page/home_page/OverView_components/Poster.jsx
@@ -4,6 +4,22 @@ export default function Poster({game}){
     return(
         <div className="poster">
         <h1>{game.name}</h1>
+        <p className="game_summary">
+            {game.summary}
+        </p>
+        {/* <h3 className="game_m_title">Game mode:</h3> */}
+        {
+            game.game_modes == null 
+            ? 'no game modes' 
+            :game.game_modes.map((g_mode)=><span className="g_mode">{g_mode}</span>)
+        }
+        {/* <br />
+        <span className="game_m_title">Genres:</span>
+        {
+            game.genres == null 
+            ? 'no genres' 
+            :game.genres.map((g_mode)=><span className="g_mode">{g_mode}</span>)
+        } */}
         </div>
     )
 }
