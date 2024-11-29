7354448f51037ee0d845c398fc17b2385ce70d61
Cherkaoui El Wafi <elwaficherkaoui@gmail.com>
2024-11-29 02:05:07 +0100

    Create games slice

--- src/redux_store/store.js
+++ src/redux_store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import user_reducer from "./user/userSlice"
+import games_reducer from "./games/gamesSlice"
 
 
 export const store = configureStore({
     reducer: {
-        user: user_reducer
+        user: user_reducer,
+        games: games_reducer
     }
 })
