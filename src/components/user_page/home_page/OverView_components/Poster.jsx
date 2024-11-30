da19055ade40b5d8d02605a86b31e6f4590d653e
Cherkaoui El Wafi <elwaficherkaoui@gmail.com>
2024-11-30 23:21:55 +0100

    Change Navbar layout, Home page styles & fonts

--- src/components/user_page/home_page/OverView_components/Poster.jsx
+++ src/components/user_page/home_page/OverView_components/Poster.jsx
@@ -3,15 +3,23 @@ export default function Poster({game}){
     let b = game.screenshots[0]
     return(
         <div className="poster">
-        <h1>{game.name}</h1>
+        <div className="game_title">
+            <h1>
+                {game.name}
+            </h1>
+            <span className="rating">{game.rating.toFixed(2)}</span>
+        </div>
+
         <p className="game_summary">
             {game.summary}
         </p>
         {/* <h3 className="game_m_title">Game mode:</h3> */}
+        
+        <span className="attr_title">Genres:</span>
         {
-            game.game_modes == null 
+            game.genres == null 
             ? 'no game modes' 
-            :game.game_modes.map((g_mode)=><span className="g_mode">{g_mode}</span>)
+            :game.genres.map((g_mode)=><span className="g_mode">{g_mode}</span>)
         }
         {/* <br />
         <span className="game_m_title">Genres:</span>
@@ -20,6 +28,7 @@ export default function Poster({game}){
             ? 'no genres' 
             :game.genres.map((g_mode)=><span className="g_mode">{g_mode}</span>)
         } */}
+        <button className="check_game_btn">Check Game</button>
         </div>
     )
 }
