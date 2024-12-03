8cdb304ac53581387d9abdf64ad02c3329023be9
hamdi touballi <hamditouballi2002@gmail.com>
2024-12-04 00:43:12 +0100

    add category

--- src/components/user_page/home_page/Categorys.jsx
+++ src/components/user_page/home_page/Categorys.jsx
@@ -37,6 +37,129 @@ export default function Categorys() {
       </div>
       
       </div>
+
+      <div className="category">
+        
+            <h1 className='title'>Role-Playing</h1>
+            <div className="category-cont">
+      {data.map((d) => {
+        if(d.genres && d.genres.includes("Role-playing (RPG)")){
+        return <Category key={d.id} game={d} />
+        }
+      })}
+      </div>
+      </div>
+
+
+      <div className="category">
+        
+            <h1 className='title'>Simulation</h1>
+            <div className="category-cont">
+      {data.map((d) => {
+        if(d.genres && d.genres.includes("Simulator")){
+        return <Category key={d.id} game={d} />
+        }
+      })}
+      </div>
+      </div>
+
+
+      <div className="category">
+        
+            <h1 className='title'>Strategy</h1>
+            <div className="category-cont">
+      {data.map((d) => {
+        if(d.genres && d.genres.includes("Strategy")){
+        return <Category key={d.id} game={d} />
+        }
+      })}
+      </div>
+      </div>
+
+      <div className="category">
+        
+            <h1 className='title'>Racing</h1>
+            <div className="category-cont">
+      {data.map((d) => {
+        if(d.genres && d.genres.includes("Racing")){
+        return <Category key={d.id} game={d} />
+        }
+      })}
+      </div>
+      </div>
+
+      <div className="category">
+        
+            <h1 className='title'>Sports</h1>
+            <div className="category-cont">
+      {data.map((d) => {
+        if(d.genres && d.genres.includes("Sport")){
+        return <Category key={d.id} game={d} />
+        }
+      })}
+      </div>
+      </div>
+
+      <div className="category">
+        
+            <h1 className='title'>Puzzle</h1>
+            <div className="category-cont">
+      {data.map((d) => {
+        if(d.genres && d.genres.includes("Puzzle")){
+        return <Category key={d.id} game={d} />
+        }
+      })}
+      </div>
+      </div>
+
+
+      <div className="category">
+        
+            <h1 className='title'>Horror</h1>
+            <div className="category-cont">
+      {data.map((d) => {
+        if(d.themes && d.themes.includes("Horror")){
+        return <Category key={d.id} game={d} />
+        }
+      })}
+      </div>
+      </div>
+
+      <div className="category">
+        
+            <h1 className='title'>Sandbox</h1>
+            <div className="category-cont">
+      {data.map((d) => {
+        if(d.themes && d.themes.includes("Sandbox")){
+        return <Category key={d.id} game={d} />
+        }
+      })}
+      </div>
+      </div>
+
+      <div className="category">
+        
+            <h1 className='title'>Open world</h1>
+            <div className="category-cont">
+      {data.map((d) => {
+        if(d.themes && d.themes.includes("Open world")){
+        return <Category key={d.id} game={d} />
+        }
+      })}
+      </div>
+      </div>
+
+      <div className="category">
+        
+            <h1 className='title'>Music</h1>
+            <div className="category-cont">
+      {data.map((d) => {
+        if(d.themes && d.themes.includes("Music")){
+        return <Category key={d.id} game={d} />
+        }
+      })}
+      </div>
+      </div>
       
     </div>
   );
