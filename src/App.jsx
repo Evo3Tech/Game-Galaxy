112f923d9385cebe8e5c884664b2128bf2aa1ebd
Cherkaoui El Wafi <elwaficherkaoui@gmail.com>
2024-12-05 22:01:20 +0100

    Improve: Hero page animations, styling, images

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Content } from './components/hero_page/Content'
 import { useSelector } from 'react-redux'
 import User_App from './components/user_page/UserApp'
 import { Hero_page } from './components/hero_page/Hero_page'
-
+import './animations_css/heropage_animations.css'
 function App() {
   const user_info = useSelector((state)=>state.user.info)
   if(user_info){
@@ -17,6 +17,8 @@ function App() {
   
   return (
     <>
+      <div className="background_img">
+      </div>
       <Navbar />
       <Content/>
     </>
--- src/components/hero_page/Navbar.jsx
+++ src/components/hero_page/Navbar.jsx
@@ -1,14 +1,16 @@
-import { useNavigate } from "react-router-dom"
+import { useLocation, useNavigate, useParams } from "react-router-dom"
 
 export function Navbar(){
     const navigate = useNavigate()
+    const current_page = useLocation().pathname
+    
     return(
         <nav>
             <h1>Game Galaxy</h1>
             <ul className="navbar">
-                <li onClick={()=>{navigate('/')}}>home</li>
-                <li onClick={()=>{navigate('/login')}}>Log In</li>
-                <li onClick={()=>{navigate('/signup')}}>Sign Up</li>
+                <li className={current_page == '/' ? "active": ""} onClick={()=>{navigate('/')}}>home</li>
+                <li className={current_page == '/login' ? "active": ""} onClick={()=>{navigate('/login')}}>Log In</li>
+                <li className={current_page == '/signup' ? "active": ""} onClick={()=>{navigate('/signup')}}>Sign Up</li>
             </ul>
         </nav>
     )
