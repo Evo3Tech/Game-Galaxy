a4f7a08b98131c3fef9ce4c4572eb9c3c162b264
Cherkaoui El Wafi <elwaficherkaoui@gmail.com>
2024-12-08 21:25:54 +0100

    Fix: Favorite functionality Add/remove

--- src/components/user_page/home_page/Favoritesbtn.jsx
+++ src/components/user_page/home_page/Favoritesbtn.jsx
@@ -1,35 +1,51 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { favorites } from '../../../redux_store/user/userSlice'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { favorites, rm_favorites } from '../../../redux_store/user/userSlice'
 export default function Favoritesbtn({game,user}) {
   const dispatch = useDispatch()
+  const favorites_list = useSelector((state)=> state.user.info.favorites)
+  const [in_favorite, setInFavorite] = useState(favorites_list.includes(game.id))
+  
   async function hundlerclick(){
-      let infoFavorit={
-        Username: user.info.name,
-        Game: game
+    let infoFavorit={
+      Username: user.info.name,
+      Game: game
+    }
+    try {
+      const response = await fetch("http://localhost:1231/favorite", {
+          method: "POST",
+          headers: {
+              'Content-Type' : 'application/json'
+          },
+          body: JSON.stringify(infoFavorit)
+      })
+      if(response.status == 201){
+        dispatch(favorites(game.id))
+        setInFavorite(true)
       }
-      try {
-        const response = await fetch("http://localhost:1231/favorite", {
-            method: "POST",
-            headers: {
-                'Content-Type' : 'application/json'
-            },
-            body: JSON.stringify(infoFavorit)
-        })
-        if(response.ok){
-            dispatch(favorites(await response.json()))
-            
-        }
-        else{
-            throw new Error(await response.text())
-        }
-    } catch (error) {
+      else if(response.status == 202){
+        dispatch(rm_favorites(game.id))
+
+        setInFavorite(false)
+      }
+      else{
+          throw new Error(await response.text())
+      }
+    } 
+    catch (error) {
         console.log(error);
     }
+    
   }
   return (
-    <div>
-        <button className="favorite_btn" onClick={hundlerclick}>favorite</button>
-    </div>
+    <button className={in_favorite ? "favorite_btn active" : "favorite_btn"} onClick={hundlerclick}>
+      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+        <g id="SVGRepo_iconCarrier"> 
+          <path d="M4.45067 13.9082L11.4033 20.4395C11.6428 20.6644 11.7625 20.7769 11.9037 20.8046C11.9673 20.8171 12.0327 20.8171 12.0963 20.8046C12.2375 20.7769 12.3572 20.6644 12.5967 20.4395L19.5493 13.9082C21.5055 12.0706 21.743 9.0466 20.0978 6.92607L19.7885 6.52734C17.8203 3.99058 13.8696 4.41601 12.4867 7.31365C12.2913 7.72296 11.7087 7.72296 11.5133 7.31365C10.1304 4.41601 6.17972 3.99058 4.21154 6.52735L3.90219 6.92607C2.25695 9.0466 2.4945 12.0706 4.45067 13.9082Z" fill="currentColor" stroke="currentColor" stroke-width="2"></path> 
+          </g>
+      </svg>
+    </button>
   )
 }
