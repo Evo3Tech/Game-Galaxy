845aa7c5833a29acc23cd143e81733a486189d7f
Cherkaoui El Wafi <elwaficherkaoui@gmail.com>
2024-12-12 00:02:44 +0100

    Style: improve UI profile components

--- src/components/user_page/home_page/Profile/Freinds.jsx
+++ src/components/user_page/home_page/Profile/Freinds.jsx
@@ -9,11 +9,13 @@ export default function Freinds() {
   return (
     <div className="freinds">
       <div className="header">
-        <h1>Followers</h1>
-        </div>
-      <hr />
+        <h1>I follow :</h1>
+      </div>
       <div className="fixlayoute">
-      {userFreinds.map((friend) => (
+        {
+        userFreinds.length == 0 
+        ? <span>You do not follow anyone</span> 
+        :userFreinds.map((friend) => (
         <div key={friend.id} className="freinds-list">
           <img src="https://easy-peasy.ai/cdn-cgi/image/quality=80,format=auto,width=700/https://fdczvxmwwjwpwbeeqcth.supabase.co/storage/v1/object/public/images/50dab922-5d48-4c6b-8725-7fd0755d9334/3a3f2d35-8167-4708-9ef0-bdaa980989f9.png"
            alt={friend.name} />
