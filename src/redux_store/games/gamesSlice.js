4565b09e9580b298688cf02b4b3144531cd17ec0
Cherkaoui El Wafi <elwaficherkaoui@gmail.com>
2025-01-06 23:37:57 +0100

    Fix: await isnt supported on higher levels

--- src/redux_store/games/gamesSlice.js
+++ src/redux_store/games/gamesSlice.js
@@ -1,11 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-let all_games = await fetch('http://localhost:1231/all_Games')
-    .then((res)=>res.json())
 export const gamesSlice = createSlice(
     {
         name: 'games',
-        initialState: all_games,
+        initialState: async ()=>{await fetch('http://localhost:1231/all_Games').then((res)=>res.json())},
         reducers: {
             get_all_games: (state, action) => {state = action.payload}
         }
